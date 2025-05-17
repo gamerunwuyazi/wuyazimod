ccf723be6003aaf491b389619e4791e76dcc98be
jiggy <wuyazi2025@outlook.com>
2025-05-17 14:16:42 +0800

    删除了Mixin

--- src/main/java/name/modid/mixin/AbstractFurnaceBlockEntityMixin.java
+++ src/main/java/name/modid/mixin/AbstractFurnaceBlockEntityMixin.java
@@ -1,13 +1,15 @@
-package name.modid.mixin;
+/*
+ package name.modid.mixin;
+  <p>
+ import org.spongepowered.asm.mixin.Mixin;
+ import org.spongepowered.asm.mixin.injection.At;
+ import org.spongepowered.asm.mixin.injection.Inject;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-
-@Mixin(AbstractFurnaceBlockEntityMixin.class)
-public class AbstractFurnaceBlockEntityMixin {
-    @Inject(at = @At("TAIL"),method = "createFuelTimeMap")
+ @Mixin(AbstractFurnaceBlockEntityMixin.class)
+ public class AbstractFurnaceBlockEntityMixin {
+    @Inject(at = @At("TAIL"),method = "")
     private static void addFuelItems() {
-
+  <p>
     }
-}
+ }
+*/
